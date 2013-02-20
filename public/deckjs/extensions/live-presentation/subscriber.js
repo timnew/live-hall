f788012d591e8023ffbb8ca534eb49fd39b3ece9
DD <timnew.wti@gmail.com>
2013-02-20 13:23:21 +0800

    Update subscriber to avoid loopback storm

--- public/deckjs/extensions/live-presentation/subscriber.js
+++ public/deckjs/extensions/live-presentation/subscriber.js
@@ -1,9 +1,16 @@
-$(function(){
-  var source = new EventSource('/live');
-  source.addEventListener('message', function (e){
-    data = JSON.parse(e.data)
-    console.log("SSE: [" + e.type + "] "+ e.data);
-    page = parseInt(data.to, 10);
-    $.deck('go', page);
-  });
+$(function () {
+    var currentPage;
+    $(document).bind('deck.change', function (event, from, to) {
+        currentPage = to;
+    });
+    var source = new EventSource('/live');
+    source.addEventListener('message', function (e) {
+        data = JSON.parse(e.data)
+        console.log("SSE: [" + e.type + "] " + e.data);
+        page = parseInt(data.to, 10);
+        if (data.to != currentPage) {
+            $.deck('go', page);
+        }
+    });
+
 });
